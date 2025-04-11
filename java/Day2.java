4b0f89ea2d9ba3b6afc77ae0508afe75e618c6ef
PRAVESH TIWARI <pravesht252@gmail.com>
2025-04-11 10:06:27 +0530

    hii

---
+++ java/Day2.java
@@ -0,0 +1,244 @@
+// Day 2
+//09/04/2025
+
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
+        //  byte a =20;
+        //  byte b=30;
+        //  byte c= (byte)a+b;
+         //jab bhi operator hum use karte hai to uska run integer value aayega 
+         //isliye usme integer value store nahi ho sakti
+         
+        //  System.out.println(10+10+"hiii");
+
+       // System.out.println("hiii"+10+10);
+
+    //    boolean a=false;
+    //    boolean b=true;
+
+
+    //    System.out.println(a&&b);
+
+
+    //    int a=5;
+    //    int b=4;
+
+    //    System.out.println(a&b);
+
+//    boolean a =false;
+//     if (a==true){
+//         System.out.println("true");
+//     }
+//     else {
+//         System.out.println("false");
+//     }
+
+
+    //    System.out.println(5<6?"hii":"Hello");
+
+
+
+
+    //. Addition, Subtraction, Multiplication, Division of two numbers
+
+
+    // int a =20,b=10;
+
+    // System.out.println("sum of two number: "+(a+b));
+
+    // System.out.println("subtraction of two number: "+(a-b));
+
+    // System.out.println("Multiplication of two number: "+(a*b));
+
+    // System.out.println("division of two number: "+(a/b));
+
+    // System.out.println("remainder of two number: "+(a%b));
+
+
+
+   // Swap two numbers using a third variable
+
+//    int a,b,temp;
+
+//      a=10;
+//      b=20;
+
+//    temp=a;
+//    a=b;
+//    b=temp;
+
+//    System.out.println(a);
+//    System.out.println(b);
+
+
+
+
+   //Swap two numbers without using a third variable
+
+//    int a,b;
+//    a=10;
+//    b=20;
+   
+//    a=a+b;
+//    b=a-b;
+//    a=a-b;
+
+
+//    System.out.println(a);
+//    System.out.println(b);
+
+
+
+//5. Check whether a number is even or odd
+
+// int num=7;
+// if(num%2==0){
+//     System.out.println("Even");
+// }else{
+//     System.out.println("ODD");
+// }
+
+
+
+
+//6. Check if a number is positive, negative, or zero
+//   int num=-7;
+
+//   if(num>0){
+//     System.out.println("Positive");
+//   }else if(num<0){
+//     System.out.println("Negative");
+
+//   }else{
+//     System.out.println("Zero");
+//    }
+
+
+//7. Find the largest of two numbers
+
+// int a=10;
+// int b=20;
+
+// if(a>b){
+//     System.out.println("largest number is a "+(a));
+// }else{
+//     System.out.println("largest number is b "+(b));
+// }
+  
+
+//8. Find the largest of three numbers
+
+// int a,b,c;
+// a=10;
+// b=20;
+// c=5;
+
+// if(a>b&&a>c)
+// {
+//     System.out.println("largest number is a "+(a));
+// }else if(b>a && b>c){
+//     System.out.println("largest number is b "+(b));
+// }else{
+//     System.out.println("largest number is c "+(c));
+// }
+
+
+
+//9. Check if a year is a leap year
+
+// int year =2025;
+
+// if ((year%4==0) && (year % 100 != 0 || year % 400 == 0)){
+//     System.out.println("Leap year");
+// }else{
+//     System.out.println("Not a Leap year");
+// }
+
+
+
+//10. Check if a number is divisible by 5 and 11
+//  int num =80;
+//  if(num%5==0 && num%11==0){
+//     System.out.println("divisible by 5 and 11");
+//  }else{
+//     System.out.println("Not divisible by 5 and 11 ");
+//  }
+
+
+//11.Check whether a character is a vowel or consonant
+    // char c ='d';
+    
+    // if(c=='a'|| c=='e' || c=='i'|| c=='o'|| c=='u' ){
+    //     System.out.println("character is vowel");
+    // }else{
+    //     System.out.println("character is consonant");
+    // }
+
+
+
+    //12.. Check whether a number is prime or not
+
+    // int num =9;
+    // boolean isTrue = true; 
+    // for(int i=2; i<=num/2; i++){
+    //     if(num % i == 0){
+    //         isTrue = false;
+    //         break;
+    //     }
+    // }
+    // if(isTrue == true){
+    //     System.out.println("Prime Number");
+    // }
+    // else{
+    //     System.out.println("Not a prime number");
+    // }
+
+
+
+    //13. Check eligibility to vote
+    // int age =25;
+
+    // if(age>18){
+    //     System.out.println("eligibility to vote");
+
+    // }else{
+    //     System.out.println("Not eligibility to vote");
+    // }
+
+
+    //14. Find grade of a student based on marks
+
+    // int marks=85;
+
+    // if(marks>75)
+    // {
+    //     System.out.println("grade a");
+    // }else if(marks>60){
+    //     System.out.println("grade b");
+    // }else if(marks>50){
+    //     System.out.println("grade c");
+    // }else if(marks>40){
+    //     System.out.println("grade d");
+    // }else{
+    //     System.out.println("fail");
+    // }
+
+    //15. Check whether a character is uppercase or lowercase
+
+//     char ch = 'A';
+// if (ch >= 'A' && ch <= 'Z')
+//  System.out.println("Uppercase");
+// else if (ch >= 'a' && ch <= 'z')
+//  System.out.println("Lowercase");
+// else
+//  System.out.println("Not an alphabet");
+
+
+// }
+    
+    
+}
+}
---
+++ java/Day3.java
@@ -0,0 +1,35 @@
+//Day 3
+//11-04-2025
+
+
+public class Day3 {
+    public static void main(String[] args) {
+        int a=5;
+
+        switch (a){
+            case 1:
+            System.out.println("hello");
+            break;
+
+            case 2:
+            System.out.println("hii");
+            break;
+
+            case 3:
+            System.out.println("kaise ho");
+            break;
+
+            case 4:
+            System.out.println("kya kar rahe ho");
+            break;
+
+            case 5:
+            System.out.println("bhai");
+            break;
+            
+            default:
+
+            System.out.println("byyy");
+        }
+    }
+}
