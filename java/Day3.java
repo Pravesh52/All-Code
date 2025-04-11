f04edd7773b07002e8655b9b2a9c8fed785ce8a3
PRAVESH TIWARI <pravesht252@gmail.com>
2025-04-11 10:41:41 +0530

    dd

--- java/Day3.java
+++ java/Day3.java
@@ -1,35 +1,122 @@
 //Day 3
 //11-04-2025
 
-
+//import java.util.*;
+import java.util.Scanner;
 public class Day3 {
     public static void main(String[] args) {
-        int a=5;
+        
 
-        switch (a){
-            case 1:
-            System.out.println("hello");
-            break;
+        // Scanner in= new Scanner(System.in);
 
-            case 2:
-            System.out.println("hii");
-            break;
+        // System.out.println("Enter you input : ");
 
-            case 3:
-            System.out.println("kaise ho");
-            break;
+        // int a= in.nextInt();
 
-            case 4:
-            System.out.println("kya kar rahe ho");
-            break;
 
-            case 5:
-            System.out.println("bhai");
-            break;
+
+        // switch (a){
+        //     case 1:
+        //     System.out.println("hello");
+        //     break;
+
+        //     case 2:
+        //     System.out.println("hii");
+        //     break;
+
+        //     case 3:
+        //     System.out.println("kaise ho");
+        //     break;
+
+        //     case 4:
+        //     System.out.println("kya kar rahe ho");
+        //     break;
+
+        //     case 5:
+        //     System.out.println("bhai");
+        //     break;
             
-            default:
+        //     default:
 
-            System.out.println("byyy");
+        //     System.out.println("byyy");
+        // }
+
+
+        // Day print
+
+
+        // switch (a){
+        //     case 1:
+        //     System.out.println("Monday");
+        //     break;
+
+        //     case 2:
+        //     System.out.println("Tuesday");
+        //     break;
+
+        //     case 3:
+        //     System.out.println("Wednesday");
+        //     break;
+
+        //     case 4:
+        //     System.out.println("Thursday");
+        //     break;
+
+        //     case 5:
+        //     System.out.println("Friday");
+        //     break;
+
+        //     case 6:
+        //     System.out.println("Saturday");
+        //     break;
+
+        //     case 7:
+        //     System.out.println("Sunday");
+        //     break;
+            
+        //     default:
+
+        //     System.out.println("invalid input");
+        // }
+
+        //Calculator
+
+        Scanner in =  new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int a= in.nextInt();
+
+        System.out.println("Enter Second number: ");
+        int b=in.nextInt();
+
+        System.out.println("Enter operator: ");
+        char c=in.next().charAt(0);
+
+
+        switch (c) {
+            case '+':
+             System.out.println("Addition: "+(a+b));
+            break;
+
+            case '-':
+             System.out.println("Addition: "+(a-b));
+            break;
+
+            case '*':
+             System.out.println("Addition: "+(a*b));
+            break;
+
+            case '/':
+             System.out.println("Addition: "+(a/b));
+            break;
+        
+            default:
+            System.out.println("default input");
+                break;
         }
+
+
+
+
     }
 }
