6f47c8477b16308ff711cbf469df716007f5bd2b
PRAVESH TIWARI <pravesht252@gmail.com>
2025-04-11 13:16:54 +0530

    aa

--- java/Day3.java
+++ java/Day3.java
@@ -188,24 +188,258 @@ public class Day3 {
         //     a++;
         // }
 
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
 
-        int x= in.nextInt();
+        // int x= in.nextInt();
 
-        //print the value using for loop
+        // //print the value using for loop
 
-        for(int i=1;i<=x;i++)
-        {
-            System.out.println(i);
-        }
+        // for(int i=1;i<=x;i++)
+        // {
+        //     System.out.println(i);
+        // }
 
-        //print the value using while loop
+        // //print the value using while loop
 
-        int i=1;
-        while(i<=x){
-            System.out.println(i);
-            i++;
-        }
+        // int i=1;
+        // while(i<=x){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+
+        // //Pattern Printing 
+
+        // Scanner in = new Scanner(System.in);
+
+
+        // System.out.println("Enter the number: ");
+
+        // int x=in.nextInt();
+       
+
+        // // for(int i=1;i<=x;i++)
+        // // {
+        // //     for(int j=1;j<=i;j++)
+        // //     {
+        // //         System.out.print("*");
+        // //     }
+        // //     System.out.println(" ");
+        // // }
+
+
+        // for(int i=x;i>=1;i--)
+        // {
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+
+        //PDF Today programming question
+
+       // 1. Check Even or Odd using if-else
+       //  Input: Any integer
+       //  Output: "Even" or "Odd"
+       //Scanner in = new Scanner(System.in);
+
+
+        // System.out.println("input 1st integer : ");
+
+        // int x=in.nextInt();
+
+        // System.out.println("input 2nd integer : ");
+
+        // int y=in.nextInt();
+
+        // if(x%2==0){
+        //     System.out.println("Even");
+        // }else{
+        //     System.out.println("odd");
+        // }
+
+    // 2. Check Positive, Negative or Zero
+    //  Input: Any integer
+    //  Output: "Positive", "Negative" or "Zero"
+    
+
+    // if(x>0)
+    // {
+    //     System.out.println("Positive");
+    // }else if(x<0){
+    //     System.out.println("Negative");
+    // }else{
+    //     System.out.println("zero");
+    // }
+
+
+    //3.Find Greatest of Two Numbers using if-else
+    //  if(x>y){
+    //     System.out.println("1st number is greatest:");
+    //  }else{
+    //     System.out.println("2nd number is greatest");
+    //  }
+
+
+    //4.Check whether a character is vowel or consonant using switch-case
+    // System.out.println("input a character : ");
+
+    
+
+    // char c=in.next().charAt(0);
+    // switch (c) {
+    //     case 'a':
+    //     case 'e':
+    //     case 'i':
+    //     case 'o':
+    //     case 'u':
+    //     System.out.println("vowel");
+    //     break;
+    
+    //     default:
+    //     System.out.println("consonent");
+    //         break;
+    // }
+
+
+
+    //5. Print numbers from 1 to N using a for loop
+    // Scanner in = new Scanner(System.in);
+
+
+    // System.out.println("input The number: ");
+
+    //  int x=in.nextInt();
+
+    //  for(int i=1;i<=x; i++)
+    //  {
+    //     System.out.println(i);
+        
+    //  }
+
+
+    //6. Sum of first N natural numbers using while loop
+    // Scanner in = new Scanner(System.in);
+
+
+    //  System.out.println("input The number: ");
+
+    //  int n=in.nextInt();
+
+    //  int summ=0;
+
+    //  int i=1;
+
+    //  while(i<=n)
+    //  {
+    //     summ+=i;
+    //     i++;
+    //  }
+    //  System.out.println("sum: "+summ);
+      
+    // 7.Print multiplication table of a number using loop
+
+    // Scanner in = new Scanner(System.in);
+
+
+    //  System.out.println("input The number: ");
+
+    //  int n=in.nextInt();
+
+    //  for(int i=1;i<=n;i++){
+    //     System.out.println(i*n);
+    //  }
+
+    //8.  Check whether a number is divisible by 5 and 11 using if-else
+
+    // Scanner in = new Scanner(System.in);
+
+
+    //  System.out.println("input The number: ");
+
+    //  int n=in.nextInt();
+
+    //  if(n/5==0 && n/11==0){
+    //     System.out.println("Divisible by 5 and 11");
+    //  }else{
+    //     System.out.println("not Divisible by 5 and 11");
+    //  }
+
+    //9. Type casting example: Convert float to int and print both
+
+    // Scanner in = new Scanner(System.in);
+
+
+    //  System.out.println("input The number: ");
+
+    //  float n=in.nextFloat();
+
+    //  System.out.println((int)n);
+
+
+
+    //10. Check whether a number is a multiple of 3 or not using if-else
+    // Scanner in = new Scanner(System.in);
+
+
+    //  System.out.println("input The number: ");
+
+    //  int n=in.nextInt();
+
+    //  if(n%3==0){
+    //     System.out.println("yes ");
+    //  }else{
+    //     System.out.println("no");
+    //  }
+
+
+
+    //11.Take a character input and print its ASCII value (Type casting)
+    // Scanner in = new Scanner(System.in);
+
+
+    //  System.out.println("input The number: ");
+
+    //  char n=in.next().charAt(0);
+
+    //  System.out.println((int)n);
+
+
+    //12.Find factorial of a number using for loop
+
+    Scanner in = new Scanner(System.in);
+
+
+     System.out.println("input The number: ");
+
+      int n=in.nextInt();
+      int Fact=1;
+
+      for(int i=1;i<=n;i++)
+      {
+        Fact = Fact*i;
+      }
+      System.out.println(Fact);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 
 
