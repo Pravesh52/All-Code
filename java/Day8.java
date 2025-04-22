17fd7d863503dfed1c24aeb5c6697e5d47646cb1
PRAVESH TIWARI <pravesht252@gmail.com>
2025-04-22 13:04:19 +0530

    Day8

---
+++ java/Day8.java
@@ -0,0 +1,74 @@
+public class Day8 {
+    public static void main(String[] args) {
+        //sum of digit
+        
+        
+        // int sum=123;
+        // int s=0;
+        // for(int i=0;i<3;i++)
+        // {
+        //     s+=sum%10;
+        //     sum/=10;
+        // }
+        // System.out.println(s);
+
+        //no one by one even odd check
+
+    //     int n=123;
+    //     while(n!=0){
+    //         int temp=n%10;
+
+    //     if(temp%2==0){
+    //         System.out.println("Even");
+    //     }else{
+    //         System.out.println("odd");
+    //     }
+    //     n=n/10;
+    // }
+
+
+    //prime number
+
+    // int arr[]={1,2,3,4,5,67,55,89}
+    // System.out.println("Prime Numbers : ");
+    //  boolean flag;
+    // for(int i=0;i<arr.length;i++){
+
+    // }
+
+
+    // String str="hello";
+    // String str1="hiii";
+
+    // for(int i=0;i<str.length();i++){
+    //     for(int j=0;j<str1.length();j++){
+    //         if(str.charAt(i)==str1.charAt(j)){
+    //             System.out.println(str.charAt(i));
+    //             break;
+    //         }
+    //     }
+    // }
+
+    //string palindrone
+    String str="racecar";
+    int l=0,r=str.length()-1;
+    boolean isPalindrome=true;
+
+    while(l<r){
+        if(str.charAt(l) != str.charAt(r)){
+            isPalindrome=false;
+            break;
+        }
+        l++;
+        r--;
+
+    }
+    if(isPalindrome){
+        System.out.println("Palindrone");
+    }else{
+        System.out.println("Not a Palindrone");
+    }
+
+        
+    }
+}
