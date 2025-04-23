0ec2e8b84aee0a831154f8dcafc3956269658b80
PRAVESH TIWARI <pravesht252@gmail.com>
2025-04-23 14:02:44 +0530

    DAY9

--- java/java training/Day9.java
+++ java/java training/Day9.java
@@ -2,15 +2,67 @@ public class Day9 {
     public static void main(String[] args) {
         //delete the duplicate data
 
-        String str="hiii";
+        // String str="hiii";
+        // String newstr="";
+
+        // for(int i=0;i<str.length();i++){
+        //     char c=str.charAt(i);
+        //     if(newstr.indexOf(c)==-1){
+        //         newstr=newstr+c;
+        //     }
+        // }
+        // System.out.println(newstr+" ");
+
+
+        //palindrone
+
+        // String str="Pravesh";
+
+        // int l=0,r=str.length()-1;
+        // boolean isPalindrome=true;
+
+        // while(l<r){
+        //     if(str.charAt(l)!=str.charAt(r)){
+        //         isPalindrome=false;
+        //         break;
+        //     }
+            
+        //     l++;
+        //     r--;
+
+        // }
+        // if(isPalindrome)
+        // {
+        //     System.out.println("Palindrone");
+        // }else{
+        //     System.out.println("Not Palindrone");
+        // }
+
+
+        //find first repeating character
+
+        
+        String str="happybirthday";
         String newstr="";
 
-        for(int i=0;i<str.length();i++){
+        for(int i=0;i<str.length();i++)
+        {
             char c=str.charAt(i);
-            if(newstr.indexOf(c)==-1){
+            if(newstr.indexOf(c)==-1)
+            {
                 newstr=newstr+c;
+ 
+            }else{
+                System.out.println(c);
+                break;
             }
+
         }
-        System.out.println(newstr+" ");
+
+        
+
+
+
+        
     }
 }
