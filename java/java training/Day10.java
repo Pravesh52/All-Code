0a7fc644f9f7332ccdd97dba019ab244aa5f0ed3
PRAVESH TIWARI <pravesht252@gmail.com>
2025-04-25 12:39:19 +0530

    Day10

--- java/java training/Day10.java
+++ java/java training/Day10.java
@@ -2,7 +2,7 @@
 //25-04-2025
 
 public class Day10 {
-        public static void main(String[] args) {
+        //public static void main(String[] args) {
             // int i = 1;
             // while (i++ <= 5) {
             //     if (i == 3) continue;
@@ -27,17 +27,123 @@ public class Day10 {
 
              //String capacity
 
-             StringBuilder str=new StringBuilder(10);
-             str.append("Hello hii byyy");
-             //old*2+2
-             System.out.println(str.length());
-             System.out.println(str.capacity());
+            //  StringBuilder str=new StringBuilder(10);
+            //  str.append("Hello hii byyy");
+            //  //old*2+2
+            //  System.out.println(str.length());
+            //  System.out.println(str.capacity());
 
 
 
+            //java starting
+
+            //  static void sum(int a,int b){
+            //     System.out.println("sum"+(a+b));
+            // }
+
+            //sum of digit
+            // static void calc(int n)
+            // {
+            //     int sum=0;
+            //     while(n!=0)
+            //     {
+            //         sum+=n%10;
+
+            //         n=n/10;
+            //     }
+            //     System.out.println(sum);
+            // }
+
+
+            // static int sum(int a,int b){
+            //     return a+b;
+            // }
+
+
+            // static int max_digit(int a,int b){
+            //     int max=0;
+            //     int sum =a+b;
+                
+            //     while(sum>0)
+            //     {
+            //         int temp=sum%10;
+            //         sum=sum/10;
+            //         if(temp>max){
+            //             max=temp;
+            //         }
+
+            //     }
+            //     return max;
 
 
 
-        }
+            // }
+
+            // public static void main(String[] args) {
+            //     //Day10 obj=new Day10();
+            //     //obj.sum();
+
+            //     //calc(123);
+
+            //     // int output=sum(5,7);
+            //     // System.out.println(output);
+
+            //      int b= max_digit(5,7);
+            //      System.out.println(b);
+            // }
+
+
+
+            //Types of variable 
+            //there are 3 types of variables.
+            //Local variable
+
+    // public static void main(String[] args)
+    // {
+    //     // Declared a Local Variable
+    //     int var = 10;
+
+    //     // This variable is local to this main method only
+    //     System.out.println("Local Variable: " + var);
+    // }
+
+   //static variables
+    // Declared static variable
+
+
+    // static int p = 10;
+  
+    // public static void main(String[] args)
+    // {
+
+    //     // static variable
+    //     System.out.println("NO is: " + p);
+
+    
+    // }
+
+
+    //Instance variables
+
+    int e=20;
+    public static void main(String[] args) {
+        Day10 obj=new Day10();
+        System.out.println(obj.e);
     }
+     
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+}
 
