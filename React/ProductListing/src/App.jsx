c06abdce411efc2f77cdefd2438cf0a17e00dcd5
PRAVESH TIWARI <pravesht252@gmail.com>
2025-10-10 10:27:47 +0530

    DAY

--- React/ProductListing/src/App.jsx
+++ React/ProductListing/src/App.jsx
@@ -1,59 +1,104 @@
-// import React from 'react'
-
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-
-  let [ApiData, SetData] = useState([])
-  // let [search, setSearch] = useState("")
-  //  let [filteredddData,SetFilteredddData]=     useState([])
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.products);
-        SetData(data.products)
-       
-      })
-  }, [])
-
-  function fun1(e){
-    SetData=e.target.value
-  }
-
-  function search(){
-    
-  }
-
-
-
-  return (
-   
-    <div>
-      <div id="Search">
-        <input type="text" onclick="" placeholder='Search the product' />
-        <button onclick="">Search</button>
-      </div>
-
-      {
-        
-        ApiData.map((a)=>{
-          return (
-            <div id="card">
-            <h3>{a.title}</h3>
-            <p>{a.brand}</p>
-            <p>{a.description}</p>
-            <h4>{a.price}</h4>
-            </div>
-          )
-  
+  let [apiData, setApiData] = useState([])
+  let [apiData2, setApiData2] = useState([])
+  let [searchData, setSearchData] = useState("")
+  // let [searchApi, setSearchApi] = useState([])
+    useEffect(()=>{
+        fetch("https://dummyjson.com/products").then((res)=>{
+            return res.json();
+        }).then((data)=>{
+            console.log(data);
+            setApiData(data.products);
+            setApiData2(data.products);
         })
-
+    },[])
+    // useEffect(()=>{
+    //     fetch("https://dummyjson.com/products/search?q=phone").then((res)=>{
+    //         return res.json();
+    //     }).then((data)=>{
+    //         console.log(data);
+    //         setSearchApi(data)
+    //     })
+    // },[])
+    function fun1(e){
+      setSearchData(e.target.value);
+      // console.log(searchData);
+    }
+    function search(){
+      if(searchData == ""){
+        alert("Empty Search Box")
+        return;
       }
-    </div>
+      setApiData2(apiData.filter((a)=>{
+        return a.title.includes(searchData);
+      }))
+    }
+    function sortData(x){
+      if(x == 1){
+        setApiData2([...apiData].sort((a, b)=>{
+          return a.price - b.price;
+        }))
+      }
+      else if(x == 2){
+        setApiData2([...apiData].sort((a, b)=>{
+          return b.price - a.price;
+        }))
+      }
+      else if(x == 3){
+        setApiData2([...apiData].sort((a, b)=>{
+          return a.rating - b.rating;
+        }))
+      }
+      else if(x == 4){
+        setApiData2([...apiData].sort((a, b)=>{
+          return b.rating - a.rating;
+        }))
+      }
+    }
+  return (
+    <>
+      <div className="nav">
+        <div className='navIcons'>
+          <h4>Sort By Price</h4>
+          <button onClick={()=>sortData(1)}>🔼</button>
+          <button onClick={()=>sortData(2)}>🔽</button>
+        </div>
+        <div className='navIcons'>
+          <input type="text" id='searchInput' onChange={fun1} placeholder='Search For Items'/>
+          <button id='searchBtn' onClick={search}>Click To Search</button>
+        </div>
+        <div className='navIcons'>
+          <h4>Sort By Rating</h4>
+          <button onClick={()=>sortData(3)}>🔼</button>
+          <button onClick={()=>sortData(4)}>🔽</button>
+        </div>
+      </div>
+      <br />
+      <section>
+        {
+          apiData2.map((a)=>{
+            return (
+              <div id='card'>
+                <img src={a.images[0]} alt="" />
+                <h3>{a.title}</h3>
+                <h4>Brand: {a.brand}</h4>
+                <h4>Description:</h4>
+                <p>{a.description}</p>
+                <p>Category: {a.category}</p>
+                <h4>Rating: {a.rating}⭐</h4>
+                <h4>Stock: {a.stock}</h4>
+                <h5>Price: ${a.price}</h5>
+              </div>
+            )
+          })
+        }
+      </section>
+    </>
   )
 }
 
-export default App
+export default App
