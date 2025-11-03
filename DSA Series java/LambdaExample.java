db23cfe9c4c1c997db64d2f0501a98b04eeab420
PRAVESH TIWARI <pravesht252@gmail.com>
2025-11-03 14:10:59 +0530

    DaY

--- DSA Series java/LambdaExample.java
+++ DSA Series java/LambdaExample.java
@@ -1,16 +1,20 @@
 // @FunctionalInterface
 interface Calculator {
-    int operate(int a, int b);
+    // int operate(int a, int b);
+    int reverse(int n);
 }
 
 public class LambdaExample {
     public static void main(String[] args) {
         // Lambda expression for addition
-        Calculator addition = (a, b) -> a + b;
-        System.out.println("Sum: " + addition.operate(10, 5)); // Output: Sum: 15
+        // Calculator addition = (a, b) -> a + b;
+        // System.out.println("Sum: " + addition.operate(10, 5)); // Output: Sum: 15
 
-        // Lambda expression for subtraction
-        Calculator subtraction = (a, b) -> a - b;
-        System.out.println("Difference: " + subtraction.operate(10, 5)); // Output: Difference: 5
+        // // Lambda expression for subtraction
+        // Calculator subtraction = (a, b) -> a - b;
+        // System.out.println("Difference: " + subtraction.operate(10, 5)); // Output: Difference: 5
+
+        Calculator reversenum=(n)->n%10;
+        System.out.println("Reverse: "+reversenum.reverse(12345));
     }
 }
