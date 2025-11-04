f3f0961d7a45c0d09e7378566587369501ab86ef
PRAVESH TIWARI <pravesht252@gmail.com>
2025-11-04 11:19:36 +0530

    List

---
+++ DSA Series java/collection framework/ListEx.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class ListEx {
+    public static void main(String[] args) {
+        List<String> fruits=new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Apple");
+        
+        System.out.println(fruits);
+
+        
+        fruits.remove(2);
+        System.out.println(fruits);
+        System.out.println(fruits.size());
+    }
+}
---
+++ Python2/List/P1.py
@@ -0,0 +1,10 @@
+lst=[]
+print(lst)
+
+lst2=["Apple","Mango","Banana"]
+lst2.append("five")
+lst2.insert(2,"two")
+lst2.remove('two')
+print(lst2)
+
+print(len(lst2))
