d77e355e3902027751506a2b024f328d8e4628e9
PRAVESH TIWARI <pravesht252@gmail.com>
2025-12-09 16:14:32 +0530

    DSA

---
+++ DSA Java/Arrays/Pair.java
@@ -0,0 +1,18 @@
+public class Pair {
+    public static void main(String[] args) {
+        int arr[]={1, 5, 7, -1, 5};
+        int target=6;
+        int count=0;
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]+arr[j]==target)
+                {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
