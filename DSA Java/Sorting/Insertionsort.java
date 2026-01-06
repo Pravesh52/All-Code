a530e6b96a6455a7c4dc155dd2dcc661e85821a9
PRAVESH TIWARI <pravesht252@gmail.com>
2026-01-06 11:46:52 +0530

    DSA

--- DSA Java/Sorting/Insertionsort.java
+++ DSA Java/Sorting/Insertionsort.java
@@ -1,5 +1,26 @@
 public class Insertionsort {
     public static void main(String[] args) {
-        int arr[]={1,8,7,4,5};
+
+        int arr[] = {1, 8, 7, 4, 5};
+        int n = arr.length;
+
+       
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+       
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
+
