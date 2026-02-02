2bf347802da3127b68add004f335cc43cb5a00c6
PRAVESH TIWARI <pravesht252@gmail.com>
2026-02-02 22:04:56 +0530

    DSA

--- DSA Java/Arrays/MergeInterval.java
+++ DSA Java/Arrays/MergeInterval.java
@@ -1,7 +1,7 @@
 public class MergeInterval {
     public static void main(String[] args) {
         int[][] arr = {{1, 3},{2, 6},{8, 10},{15, 18}};
-        int result
+        int result = 
 
 
     }
